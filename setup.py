102a81644cb9cffbefbe9519884cc964caad0d97
Xuechen <lxuechen@gmail.com>
2017-08-29 16:03:43 +0800

    update setup

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ setup(
     author_email='lxuechen@cs.toronto.edu',
     url='https://github.com/lxuechen/bayesian-nn',
     download_url='https://github.com/lxuechen/bayesian-nn/archive/0.1.tar.gz',
-    keywords='machine learning bayesian neural network tensorflow',
-    classifiers=['Intended Audience :: Developers',
-                 'Intended Audience :: Education',
-                 'Intended Audience :: Science/Research',
-                 'Operating System :: POSIX :: Linux',
-                 'Operating System :: MacOS :: MacOS X',
-                 'Operating System :: Microsoft :: Windows',
-                 'Programming Language :: Python :: 2.7',
-                 'Programming Language :: Python :: 3.4']
+    keywords='machine learning bayesian neural network tensorflow'
+    # classifiers=['Intended Audience :: Developers',
+    #              'Intended Audience :: Education',
+    #              'Intended Audience :: Science/Research',
+    #              'Operating System :: POSIX :: Linux',
+    #              'Operating System :: MacOS :: MacOS X',
+    #              'Operating System :: Microsoft :: Windows',
+    #              'Programming Language :: Python :: 2.7',
+    #              'Programming Language :: Python :: 3.4'],
 )
