e591948f8ddb98cede807360b7f34ff400080c27
Xuechen <lxuechen@gmail.com>
2017-08-29 15:52:43 +0800

    update setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+  name = 'bayesian-nn',
+  packages = ['bayesian-nn'], 
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Xuechen Li',
+  author_email = 'lxuechen@cs.toronto.edu',
+  url = 'https://github.com/lxuechen/bayesian-nn',
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = 'probabilistic machine learning bayesian neural network',
+  classifiers=['Intended Audience :: Developers',
+               'Intended Audience :: Education',
+               'Intended Audience :: Science/Research',
+               'License :: MIT Software License',
+               'Operating System :: POSIX :: Linux',
+               'Operating System :: MacOS :: MacOS X',
+               'Operating System :: Microsoft :: Windows',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 3.4']
+)
